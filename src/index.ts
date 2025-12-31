16717c7e835480200813de0d96d6f7cb8f38fc8c
MPucheta <smpuchetadev@gmail.com>
2025-12-31 01:36:17 -0300

    Initial Boilerplata for publishing

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import version from './versionCheck';
+
+export { version };
---
+++ src/versionCheck/index.ts
@@ -0,0 +1,7 @@
+type Version = `${string}.${string}.${string}`;
+
+function version(): Version {
+  return '0.0.1';
+}
+
+export default version;
